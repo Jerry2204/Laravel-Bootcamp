9aaf8876355f28c1524c9b9957c603a48231f141
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-11-09 23:37:52 +0700

    install and integrate socialite

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,16 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
 Route::get('/checkout', function () {
     return view('checkout');
-});
+})->name('checkout');
+
 Route::get('/success/checkout', function () {
     return view('success_checkout');
-});
-Route::get('/login', function () {
-    return view('login');
-});
+})->name('success.checkout');
+
+// socialite routes
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
